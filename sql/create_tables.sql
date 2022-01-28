cb538ae906e76179e805196ee69b29d11a3604f4
Valdemar Antunes <valdemarpca@hotmail.com>
2022-01-28 23:49:57 +0000

    create tables fix

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -2,51 +2,43 @@
 ---------------------------------------------------------------------------------------
 ---------------------------------STATE AND USER ROLES----------------------------------
 ---------------------------------------------------------------------------------------
-
-CREATE TABLE user (
-    user_id SERIAL PRIMARY KEY,
-    user_name VARCHAR,
-    user_email VARCHAR,
-    password VARCHAR
-)
-
-CREATE TABLE role (
+CREATE TABLE user_role (
     role_name VARCHAR PRIMARY KEY
-)
+);
 
-
-CREATE TABLE user_roles (
-    user_id INT,
-    role_name VARCHAR,
-    PRIMARY KEY (user_id, role_name),
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(user_id),
-    CONSTRAINT fk_role_name FOREIGN KEY (role_name) REFERENCES role(role_name)
-)
+CREATE TABLE user_account (
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR NOT NULL,
+    user_email VARCHAR NOT NULL UNIQUE,
+    password VARCHAR,
+    user_role VARCHAR,
+    CONSTRAINT fk_user_role FOREIGN KEY(user_role) REFERENCES user_role(role_name)
+);
 
 
 CREATE TABLE user_notification (
     notification_id SERIAL PRIMARY KEY,
     notified_user INT,
-    title VARCHAR,
-    description TEXT,
-    CONSTRAINT fk_notified_user FOREIGN KEY(notified_user) REFERENCES user(user_id)
-)
+    title VARCHAR NOT NULL,
+    description TEXT NOT NULL,
+    CONSTRAINT fk_notified_user FOREIGN KEY(notified_user) REFERENCES user_account(user_id)
+);
 
 
-CREATE TABLE state (
+CREATE TABLE states (
     state_name VARCHAR PRIMARY KEY,
     role_responsible_for_advancing VARCHAR,
-    CONSTRAINT fk_role FOREIGN KEY(role_responsible_for_advancing) REFERENCES role(role_name)
-)
+    CONSTRAINT fk_role FOREIGN KEY(role_responsible_for_advancing) REFERENCES user_role(role_name)
+);
 
 CREATE TABLE state_transition (
-    id SERIAL,
+    id SERIAL PRIMARY KEY,
     state_before VARCHAR,
     state_after VARCHAR,
-    transition_date DATETIME,
-    CONSTRAINT fk_state_before FOREIGN KEY (state_before) REFERENCES state(state_name),
-    CONSTRAINT fk_state_after FOREIGN KEY (state_after) REFERENCES state(state_name),
-)
+    transition_date TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_state_before FOREIGN KEY (state_before) REFERENCES states(state_name),
+    CONSTRAINT fk_state_after FOREIGN KEY (state_after) REFERENCES states(state_name)
+);
 
 
 ---------------------------------------------------------------------------------------
@@ -56,21 +48,21 @@ CREATE TABLE state_transition (
 CREATE TABLE investigation_team (
     team_id SERIAL PRIMARY KEY,
     investigator_id INT,
-    investigator_role VARCHAR, -- team member / principal
-    CONSTRAINT fk_investigator_id FOREIGN KEY (investigator_id) REFERENCES user(user_id)
-)
+    investigator_role VARCHAR NOT NULL, -- team member / principal
+    CONSTRAINT fk_investigator_id FOREIGN KEY (investigator_id) REFERENCES user_account(user_id)
+);
 
 
 ---------------------------------------------------------------------------------------
 -----------------------------------------FILES-----------------------------------------
 ---------------------------------------------------------------------------------------
 
-CREATE TABLE file (
+CREATE TABLE files (
     file_id SERIAL PRIMARY KEY,
-    file_name VARCHAR,
-    file_path TEXT,
-    file_size INT
-)
+    file_name VARCHAR NOT NULL,
+    file_path TEXT NOT NULL,
+    file_size INT NOT NULL
+);
 
 ---------------------------------------------------------------------------------------
 ---------------------------------PROPOSAL INFORMATION----------------------------------
@@ -78,16 +70,16 @@ CREATE TABLE file (
 
 CREATE TABLE pathology (
     pathology_name VARCHAR PRIMARY KEY 
-)
+);
 
 
 CREATE TABLE service (
     service_name VARCHAR PRIMARY KEY
-)
+);
 
 CREATE TABLE therapeutic_area (
     therapeutic_area_name VARCHAR PRIMARY KEY
-)
+);
 
 
 CREATE TABLE proposal (
@@ -98,17 +90,16 @@ CREATE TABLE proposal (
     therapeutic_area_id VARCHAR,
     protocol_state VARCHAR,
     investigation_team_id INT,
-    sigla VARCHAR,
-    proposal_type VARCHAR,
-    date_created DATETIME,
-    last_update DATETIME,
-    proposal_type VARCHAR,
-    CONSTRAINT fk_state_id FOREIGN KEY(state_id) REFERENCES state(state_name),
+    sigla VARCHAR NOT NULL,
+    proposal_type VARCHAR NOT NULL, --clinical trial / observational study
+    date_created TIMESTAMP DEFAULT NOW(),
+    last_update TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_state_id FOREIGN KEY(state_id) REFERENCES states(state_name),
     CONSTRAINT fk_pathology_id FOREIGN KEY(pathology_id) REFERENCES pathology(pathology_name),
     CONSTRAINT fk_therapeutic_area_id FOREIGN KEY(therapeutic_area_id) REFERENCES therapeutic_area(therapeutic_area_name),
-    CONSTRAINT fk_protocol_state FOREIGN KEY(protocol_state) REFERENCES state(state_name),
+    CONSTRAINT fk_protocol_state FOREIGN KEY(protocol_state) REFERENCES states(state_name),
     CONSTRAINT fk_investigation_team_id FOREIGN KEY(investigation_team_id) REFERENCES investigation_team(team_id)
-)
+);
 
 
 CREATE TABLE proposal_state_transitions (
@@ -117,7 +108,7 @@ CREATE TABLE proposal_state_transitions (
     PRIMARY KEY (proposal_id, state_transition_id),
     CONSTRAINT fk_proposal_id FOREIGN KEY (proposal_id) REFERENCES proposal(proposal_id),
     CONSTRAINT fk_state_transition_id FOREIGN KEY (state_transition_id) REFERENCES state_transition(id)
-)
+);
 
 
 CREATE TABLE proposal_files (
@@ -125,43 +116,44 @@ CREATE TABLE proposal_files (
     proposal_id INT,
     file_id INT,
     CONSTRAINT fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id),
-    CONSTRAINT fk_file_id FOREIGN KEY(file_id) REFERENCES file(file_id)
-)
+    CONSTRAINT fk_file_id FOREIGN KEY(file_id) REFERENCES files(file_id)
+);
 
 
-CREATE TABLE comment (
+CREATE TABLE proposal_comments (
     comment_id SERIAL PRIMARY KEY,
     proposal_id INT,
     author_id INT,
-    date_created DATETIME,
-    content TEXT,
-    comment_type VARCHAR,
-    CONSTRAINT fk_comment_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id)
-)
+    date_created TIMESTAMP DEFAULT NOW(),
+    content TEXT NOT NULL,
+    comment_type VARCHAR NOT NULL,
+    CONSTRAINT fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id),
+    CONSTRAINT fk_author_id FOREIGN KEY(author_id) REFERENCES user_account(user_id)
+);
 
 CREATE TABLE timeline_event (
     event_id SERIAL PRIMARY KEY,
     proposal_id INT,
-    event_type VARCHAR, -- deadline / state transition
-    event_name VARCHAR,
-    deadline_date DATETIME,
-    completed_date DATETIME,
+    event_type VARCHAR NOT NULL, -- deadline / states transition
+    event_name VARCHAR NOT NULL,
+    deadline_date DATE NOT NULL,
+    completed_date DATE,
     is_over_due BIT,
-    days_over_due DATETIME,
+    days_over_due INT,
     CONSTRAINT  fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id)
-)
+);
 
 
 ---------------------------------------------------------------------------------------
 -----------------------PROPOSAL FINANCIAL INFORMATION----------------------------------
 ---------------------------------------------------------------------------------------
 
-CREATE TABLE promoter_id (
+CREATE TABLE promoter (
     promoter_id SERIAL PRIMARY KEY,
-    name VARCHAR,
-    promoter_type VARCHAR,
-    email VARCHAR
-)
+    name VARCHAR NOT NULL,
+    promoter_type VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+);
 
 CREATE TABLE proposal_financial_component (
     proposal_financial_id SERIAL,
@@ -169,19 +161,19 @@ CREATE TABLE proposal_financial_component (
     financial_contract_id INT,
     promoter_id INT,
     CONSTRAINT fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id),
-    CONSTRAINT fk_financial_contract_id FOREIGN KEY (financial_contract_id) REFERENCES file(file_id),
+    CONSTRAINT fk_financial_contract_id FOREIGN KEY (financial_contract_id) REFERENCES files(file_id),
     CONSTRAINT fk_promoter_id FOREIGN KEY(promoter_id) REFERENCES promoter(promoter_id)
-)
+);
 
 CREATE TABLE partnerships (
     partnership_id SERIAL PRIMARY KEY,
     proposal_financial_id INT,
     icon_url TEXT,
-    spokesman_name VARCHAR,
-    email VARCHAR,
+    spokesman_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
     phone_contact VARCHAR,
-    site_url TEXT,
-)
+    site_url TEXT
+);
 
 
 ---------------------------------------------------------------------------------------
@@ -199,32 +191,32 @@ CREATE TABLE clinical_research (
     start_date DATE,
     end_date DATE,
     estimated_end_date DATE,
-    industry,
+    industry VARCHAR,
     protocol VARCHAR,
     initiative_by VARCHAR,
     phase VARCHAR, -- phase 1 | 2 | 3 | 4 
     CONSTRAINT fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id),
-    CONSTRAINT fk_state_id FOREIGN KEY(research_state) REFERENCES state(state_name)
-)
+    CONSTRAINT fk_state_id FOREIGN KEY(research_state) REFERENCES states(state_name)
+);
 
 CREATE TABLE research_state_transitions (
     research_id INT,
     state_transition_id INT,
-    motive text, -- reason for canceling / reason for starting such as SIV completed
+    motive text NOT NULL, -- reason for canceling / reason for starting such as SIV completed
     PRIMARY KEY (research_id, state_transition_id),
     CONSTRAINT fk_research_id FOREIGN KEY (research_id) REFERENCES clinical_research(research_id),
     CONSTRAINT fk_state_transition_id FOREIGN KEY (state_transition_id) REFERENCES state_transition(id)
-)
+);
 
 
 CREATE TABLE dossier (
     dossier_id SERIAL PRIMARY KEY,
     clinical_research_id INT,
-    volume VARCHAR,
-    label VARCHAR,
-    amount INT,
+    volume VARCHAR NOT NULL,
+    label VARCHAR NOT NULL,
+    amount INT NOT NULL,
     CONSTRAINT fk_clinical_research FOREIGN KEY(clinical_research_id) REFERENCES clinical_research(research_id)
-)
+);
 
 
 CREATE TABLE scientific_activities (
@@ -237,10 +229,10 @@ CREATE TABLE scientific_activities (
     volume_number INT,
     paper_num_pages INT,
     country_published VARCHAR,
-    index BIT,
+    has_been_indexed BIT,
     published_url TEXT,
     CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id)
-)
+);
 
 ---------------------------------------------------------------------------------------
 --------------------------------RESEARCH PARTICIPANTS----------------------------------
@@ -248,20 +240,20 @@ CREATE TABLE scientific_activities (
 
 CREATE TABLE participant (
     process_id BIGINT PRIMARY KEY,
-    full_name VARCHAR,
-    gender VARCHAR,
-    age INT
-)
+    full_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    age INT NOT NULL
+);
 
 
 CREATE TABLE research_participants (
     participant_id BIGINT,
     research_id INT,
-    join_date DATETIME,
+    join_date TIMESTAMP DEFAULT NOW(),
     PRIMARY KEY (participant_id, research_id),
     CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id),
     CONSTRAINT fk_participant_id FOREIGN KEY(participant_id) REFERENCES participant(process_id)
-)
+);
 
 
 ---------------------------------------------------------------------------------------
@@ -272,15 +264,17 @@ CREATE TABLE clinical_visit (
     visit_id SERIAL PRIMARY KEY,
     research_id INT,
     participant_id INT,
-    visit_type VARCHAR,
-    start_date DATETIME,
-    end_date DATETIME,
+    visit_type VARCHAR NOT NULL,
+    scheduled_date TIMESTAMP NOT NULL,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    periodicy VARCHAR,
     observations TEXT,
     hasAdverseEventAlert BIT,
     has_marked_attendance BIT,
     CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id),
     CONSTRAINT fk_participant_id FOREIGN KEY(participant_id) REFERENCES participant(process_id)
-)
+);
 
 
 CREATE TABLE visit_assigned_investigators (
@@ -288,8 +282,8 @@ CREATE TABLE visit_assigned_investigators (
     investigator_id INT,
     PRIMARY KEY (visit_id, investigator_id),
     CONSTRAINT fk_visit_id FOREIGN KEY(visit_id) REFERENCES clinical_visit(visit_id),
-    CONSTRAINT fk_investigator_id FOREIGN KEY(investigator_id) REFERENCES user(user_id)
-)
+    CONSTRAINT fk_investigator_id FOREIGN KEY(investigator_id) REFERENCES user_account(user_id)
+);
 
 ---------------------------------------------------------------------------------------
 ------------------------CLINICAL TRIAL FINANCIAL COMPONENTS----------------------------
@@ -298,36 +292,35 @@ CREATE TABLE visit_assigned_investigators (
 CREATE TABLE trial_financial_component (
     financial_id SERIAL PRIMARY KEY,
     research_id INT,
-    research_finance INT,
     value_per_participant INT,
     role_value_per_participant INT,
     balance FLOAT,
-    CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id),
-)
+    CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id)
+);
 
 CREATE TABLE team_finance (
     team_finance_id SERIAL PRIMARY KEY,
     trial_financial_component_id INT,
     investigator_id INT, 
-    type_of_flow VARCHAR,
-    transaction_date DATETIME,
-    responsible_for_payment VARCHAR,
-    amount FLOAT,
-    partition_percentage,
-    role_amount FLOAT,
-    CONSTRAINT fk_trial_finance_component FOREIGN KEY (trial_financial_component_id) REFERENCES trial_financial_component(financial_id),
-    CONSTRAINT fk_investigator_id FOREIGN KEY (investigator_id) REFERENCES user(user_id)
-)
+    type_of_flow VARCHAR NOT NULL,
+    transaction_date TIMESTAMP DEFAULT NOW(),
+    responsible_for_payment VARCHAR NOT NULL,
+    amount FLOAT NOT NULL,
+    partition_percentage NUMERIC(5,2) NOT NULL,
+    role_amount FLOAT NOT NULL,
+    CONSTRAINT fk_trial_financial_component FOREIGN KEY (trial_financial_component_id) REFERENCES trial_financial_component(financial_id),
+    CONSTRAINT fk_investigator_id FOREIGN KEY (investigator_id) REFERENCES user_account(user_id)
+);
 
 CREATE TABLE research_finance (
     research_finance_id SERIAL PRIMARY KEY,
-    trial_finance_component_id INT,
-    transaction_date DATETIME,
-    type_of_flow VARCHAR,
-    motive VARCHAR,
-    amount FLOAT,
-    CONSTRAINT fk_trial_finance_component FOREIGN KEY(trial_finance_component_id) REFERENCES trial_finance_component(financial_id)
-)
+    trial_financial_component_id INT,
+    transaction_date TIMESTAMP DEFAULT NOW(),
+    type_of_flow VARCHAR NOT NULL,
+    motive VARCHAR NOT NULL,
+    amount FLOAT NOT NULL,
+    CONSTRAINT fk_trial_financial_component FOREIGN KEY(trial_financial_component_id) REFERENCES trial_financial_component(financial_id)
+);
 
 
 ---------------------------------------------------------------------------------------
@@ -340,14 +333,15 @@ CREATE TABLE addenda (
     addenda_state VARCHAR,
     addenda_file_id INT,
     CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id),
-    CONSTRAINT fk_addenda_state FOREIGN KEY(addenda_state) REFERENCES state(state_name),
-    CONSTRAINT fk_file_id FOREIGN KEY(addenda_file_id) REFERENCES file(file_id)
-)
+    CONSTRAINT fk_addenda_state FOREIGN KEY(addenda_state) REFERENCES states(state_name),
+    CONSTRAINT fk_file_id FOREIGN KEY(addenda_file_id) REFERENCES files(file_id)
+);
 
 CREATE TABLE addenda_state_transitions (
     addenda_id INT,
     state_transition_id INT,
+    observations TEXT,
     PRIMARY KEY (addenda_id, state_transition_id),
     CONSTRAINT fk_addenda_id FOREIGN KEY (addenda_id) REFERENCES addenda(addenda_id),
     CONSTRAINT fk_state_transition_id FOREIGN KEY (state_transition_id) REFERENCES state_transition(id)
-)
+);
