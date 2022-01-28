17cd650a6a4041569b1b06dc65ef8b62d6d63c92
Valdemar Antunes <valdemarpca@hotmail.com>
2022-01-28 11:31:56 +0000

    create tables done

---
+++ sql/create_tables.sql
@@ -0,0 +1,353 @@
+
+---------------------------------------------------------------------------------------
+---------------------------------STATE AND USER ROLES----------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE user (
+    user_id SERIAL PRIMARY KEY,
+    user_name VARCHAR,
+    user_email VARCHAR,
+    password VARCHAR
+)
+
+CREATE TABLE role (
+    role_name VARCHAR PRIMARY KEY
+)
+
+
+CREATE TABLE user_roles (
+    user_id INT,
+    role_name VARCHAR,
+    PRIMARY KEY (user_id, role_name),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(user_id),
+    CONSTRAINT fk_role_name FOREIGN KEY (role_name) REFERENCES role(role_name)
+)
+
+
+CREATE TABLE user_notification (
+    notification_id SERIAL PRIMARY KEY,
+    notified_user INT,
+    title VARCHAR,
+    description TEXT,
+    CONSTRAINT fk_notified_user FOREIGN KEY(notified_user) REFERENCES user(user_id)
+)
+
+
+CREATE TABLE state (
+    state_name VARCHAR PRIMARY KEY,
+    role_responsible_for_advancing VARCHAR,
+    CONSTRAINT fk_role FOREIGN KEY(role_responsible_for_advancing) REFERENCES role(role_name)
+)
+
+CREATE TABLE state_transition (
+    id SERIAL,
+    state_before VARCHAR,
+    state_after VARCHAR,
+    transition_date DATETIME,
+    CONSTRAINT fk_state_before FOREIGN KEY (state_before) REFERENCES state(state_name),
+    CONSTRAINT fk_state_after FOREIGN KEY (state_after) REFERENCES state(state_name),
+)
+
+
+---------------------------------------------------------------------------------------
+---------------------------------INVESTIGATION TEAM------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE investigation_team (
+    team_id SERIAL PRIMARY KEY,
+    investigator_id INT,
+    investigator_role VARCHAR, -- team member / principal
+    CONSTRAINT fk_investigator_id FOREIGN KEY (investigator_id) REFERENCES user(user_id)
+)
+
+
+---------------------------------------------------------------------------------------
+-----------------------------------------FILES-----------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE file (
+    file_id SERIAL PRIMARY KEY,
+    file_name VARCHAR,
+    file_path TEXT,
+    file_size INT
+)
+
+---------------------------------------------------------------------------------------
+---------------------------------PROPOSAL INFORMATION----------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE pathology (
+    pathology_name VARCHAR PRIMARY KEY 
+)
+
+
+CREATE TABLE service (
+    service_name VARCHAR PRIMARY KEY
+)
+
+CREATE TABLE therapeutic_area (
+    therapeutic_area_name VARCHAR PRIMARY KEY
+)
+
+
+CREATE TABLE proposal (
+    proposal_id SERIAL PRIMARY KEY,
+    state_id VARCHAR,
+    pathology_id VARCHAR,
+    service_id VARCHAR,
+    therapeutic_area_id VARCHAR,
+    protocol_state VARCHAR,
+    investigation_team_id INT,
+    sigla VARCHAR,
+    proposal_type VARCHAR,
+    date_created DATETIME,
+    last_update DATETIME,
+    proposal_type VARCHAR,
+    CONSTRAINT fk_state_id FOREIGN KEY(state_id) REFERENCES state(state_name),
+    CONSTRAINT fk_pathology_id FOREIGN KEY(pathology_id) REFERENCES pathology(pathology_name),
+    CONSTRAINT fk_therapeutic_area_id FOREIGN KEY(therapeutic_area_id) REFERENCES therapeutic_area(therapeutic_area_name),
+    CONSTRAINT fk_protocol_state FOREIGN KEY(protocol_state) REFERENCES state(state_name),
+    CONSTRAINT fk_investigation_team_id FOREIGN KEY(investigation_team_id) REFERENCES investigation_team(team_id)
+)
+
+
+CREATE TABLE proposal_state_transitions (
+    proposal_id INT,
+    state_transition_id INT,
+    PRIMARY KEY (proposal_id, state_transition_id),
+    CONSTRAINT fk_proposal_id FOREIGN KEY (proposal_id) REFERENCES proposal(proposal_id),
+    CONSTRAINT fk_state_transition_id FOREIGN KEY (state_transition_id) REFERENCES state_transition(id)
+)
+
+
+CREATE TABLE proposal_files (
+    id SERIAL PRIMARY KEY,
+    proposal_id INT,
+    file_id INT,
+    CONSTRAINT fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id),
+    CONSTRAINT fk_file_id FOREIGN KEY(file_id) REFERENCES file(file_id)
+)
+
+
+CREATE TABLE comment (
+    comment_id SERIAL PRIMARY KEY,
+    proposal_id INT,
+    author_id INT,
+    date_created DATETIME,
+    content TEXT,
+    comment_type VARCHAR,
+    CONSTRAINT fk_comment_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id)
+)
+
+CREATE TABLE timeline_event (
+    event_id SERIAL PRIMARY KEY,
+    proposal_id INT,
+    event_type VARCHAR, -- deadline / state transition
+    event_name VARCHAR,
+    deadline_date DATETIME,
+    completed_date DATETIME,
+    is_over_due BIT,
+    days_over_due DATETIME,
+    CONSTRAINT  fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id)
+)
+
+
+---------------------------------------------------------------------------------------
+-----------------------PROPOSAL FINANCIAL INFORMATION----------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE promoter_id (
+    promoter_id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    promoter_type VARCHAR,
+    email VARCHAR
+)
+
+CREATE TABLE proposal_financial_component (
+    proposal_financial_id SERIAL,
+    proposal_id INT,
+    financial_contract_id INT,
+    promoter_id INT,
+    CONSTRAINT fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id),
+    CONSTRAINT fk_financial_contract_id FOREIGN KEY (financial_contract_id) REFERENCES file(file_id),
+    CONSTRAINT fk_promoter_id FOREIGN KEY(promoter_id) REFERENCES promoter(promoter_id)
+)
+
+CREATE TABLE partnerships (
+    partnership_id SERIAL PRIMARY KEY,
+    proposal_financial_id INT,
+    icon_url TEXT,
+    spokesman_name VARCHAR,
+    email VARCHAR,
+    phone_contact VARCHAR,
+    site_url TEXT,
+)
+
+
+---------------------------------------------------------------------------------------
+----------------------------------CLINICAL RESEARCH------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE clinical_research (
+    research_id SERIAL PRIMARY KEY,
+    proposal_id INT,
+    research_state VARCHAR,
+    eudra_ct VARCHAR,
+    sample_size INT,
+    duration INT,
+    cro VARCHAR,
+    start_date DATE,
+    end_date DATE,
+    estimated_end_date DATE,
+    industry,
+    protocol VARCHAR,
+    initiative_by VARCHAR,
+    phase VARCHAR, -- phase 1 | 2 | 3 | 4 
+    CONSTRAINT fk_proposal_id FOREIGN KEY(proposal_id) REFERENCES proposal(proposal_id),
+    CONSTRAINT fk_state_id FOREIGN KEY(research_state) REFERENCES state(state_name)
+)
+
+CREATE TABLE research_state_transitions (
+    research_id INT,
+    state_transition_id INT,
+    motive text, -- reason for canceling / reason for starting such as SIV completed
+    PRIMARY KEY (research_id, state_transition_id),
+    CONSTRAINT fk_research_id FOREIGN KEY (research_id) REFERENCES clinical_research(research_id),
+    CONSTRAINT fk_state_transition_id FOREIGN KEY (state_transition_id) REFERENCES state_transition(id)
+)
+
+
+CREATE TABLE dossier (
+    dossier_id SERIAL PRIMARY KEY,
+    clinical_research_id INT,
+    volume VARCHAR,
+    label VARCHAR,
+    amount INT,
+    CONSTRAINT fk_clinical_research FOREIGN KEY(clinical_research_id) REFERENCES clinical_research(research_id)
+)
+
+
+CREATE TABLE scientific_activities (
+    activity_id SERIAL PRIMARY KEY,
+    research_id INT,
+    date_published DATE,
+    author VARCHAR,
+    paper_name VARCHAR,
+    volume VARCHAR,
+    volume_number INT,
+    paper_num_pages INT,
+    country_published VARCHAR,
+    index BIT,
+    published_url TEXT,
+    CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id)
+)
+
+---------------------------------------------------------------------------------------
+--------------------------------RESEARCH PARTICIPANTS----------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE participant (
+    process_id BIGINT PRIMARY KEY,
+    full_name VARCHAR,
+    gender VARCHAR,
+    age INT
+)
+
+
+CREATE TABLE research_participants (
+    participant_id BIGINT,
+    research_id INT,
+    join_date DATETIME,
+    PRIMARY KEY (participant_id, research_id),
+    CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id),
+    CONSTRAINT fk_participant_id FOREIGN KEY(participant_id) REFERENCES participant(process_id)
+)
+
+
+---------------------------------------------------------------------------------------
+------------------------------------TRIAL VISITS---------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE clinical_visit (
+    visit_id SERIAL PRIMARY KEY,
+    research_id INT,
+    participant_id INT,
+    visit_type VARCHAR,
+    start_date DATETIME,
+    end_date DATETIME,
+    observations TEXT,
+    hasAdverseEventAlert BIT,
+    has_marked_attendance BIT,
+    CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id),
+    CONSTRAINT fk_participant_id FOREIGN KEY(participant_id) REFERENCES participant(process_id)
+)
+
+
+CREATE TABLE visit_assigned_investigators (
+    visit_id INT,
+    investigator_id INT,
+    PRIMARY KEY (visit_id, investigator_id),
+    CONSTRAINT fk_visit_id FOREIGN KEY(visit_id) REFERENCES clinical_visit(visit_id),
+    CONSTRAINT fk_investigator_id FOREIGN KEY(investigator_id) REFERENCES user(user_id)
+)
+
+---------------------------------------------------------------------------------------
+------------------------CLINICAL TRIAL FINANCIAL COMPONENTS----------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE trial_financial_component (
+    financial_id SERIAL PRIMARY KEY,
+    research_id INT,
+    research_finance INT,
+    value_per_participant INT,
+    role_value_per_participant INT,
+    balance FLOAT,
+    CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id),
+)
+
+CREATE TABLE team_finance (
+    team_finance_id SERIAL PRIMARY KEY,
+    trial_financial_component_id INT,
+    investigator_id INT, 
+    type_of_flow VARCHAR,
+    transaction_date DATETIME,
+    responsible_for_payment VARCHAR,
+    amount FLOAT,
+    partition_percentage,
+    role_amount FLOAT,
+    CONSTRAINT fk_trial_finance_component FOREIGN KEY (trial_financial_component_id) REFERENCES trial_financial_component(financial_id),
+    CONSTRAINT fk_investigator_id FOREIGN KEY (investigator_id) REFERENCES user(user_id)
+)
+
+CREATE TABLE research_finance (
+    research_finance_id SERIAL PRIMARY KEY,
+    trial_finance_component_id INT,
+    transaction_date DATETIME,
+    type_of_flow VARCHAR,
+    motive VARCHAR,
+    amount FLOAT,
+    CONSTRAINT fk_trial_finance_component FOREIGN KEY(trial_finance_component_id) REFERENCES trial_finance_component(financial_id)
+)
+
+
+---------------------------------------------------------------------------------------
+---------------------------------------ADDENDA-----------------------------------------
+---------------------------------------------------------------------------------------
+
+CREATE TABLE addenda (
+    addenda_id SERIAL PRIMARY KEY,
+    research_id INT,
+    addenda_state VARCHAR,
+    addenda_file_id INT,
+    CONSTRAINT fk_research_id FOREIGN KEY(research_id) REFERENCES clinical_research(research_id),
+    CONSTRAINT fk_addenda_state FOREIGN KEY(addenda_state) REFERENCES state(state_name),
+    CONSTRAINT fk_file_id FOREIGN KEY(addenda_file_id) REFERENCES file(file_id)
+)
+
+CREATE TABLE addenda_state_transitions (
+    addenda_id INT,
+    state_transition_id INT,
+    PRIMARY KEY (addenda_id, state_transition_id),
+    CONSTRAINT fk_addenda_id FOREIGN KEY (addenda_id) REFERENCES addenda(addenda_id),
+    CONSTRAINT fk_state_transition_id FOREIGN KEY (state_transition_id) REFERENCES state_transition(id)
+)
